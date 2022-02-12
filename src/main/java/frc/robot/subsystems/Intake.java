12caac593d7a77a4fac3d3cf9b6743b704e2a366
TigerStrike6026 <tigerstrike6026@gmail.com>
2022-02-12 15:33:15 -0600

    Adjusting Characterization Values of Intake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -8,11 +8,12 @@ import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.SparkMaxPIDController.ArbFFUnits;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
-    private final double _runRpm = 500;
-    private final double _kv = 0.060519;
+    private final double _runRpm = 200;
+    private final double _kv = 0.060519 / 60;
     private final double _ks = 0.20893;
     private final CANSparkMax _intake = new CANSparkMax(5, MotorType.kBrushless);
     private final RelativeEncoder _intakeEncoder;
@@ -23,22 +24,35 @@ public class Intake extends SubsystemBase {
 
         _intake.restoreFactoryDefaults();
         _intake.setIdleMode(IdleMode.kCoast);
+        _intake.setInverted(true);
         _intakePID = _intake.getPIDController();
         _intakeEncoder = _intake.getEncoder();
-        _intakePID.setP(0.09379);
+        _intakePID.setP(0.048229 / 60);
         _intakePID.setOutputRange(-1, 1);
-        _intakePID.setSmartMotionMaxAccel(500, 0);
+        _intakePID.setSmartMotionMaxVelocity(500, 0);
+        _intakePID.setSmartMotionMaxAccel(100, 0);
         _intakePID.setFeedbackDevice(_intakeEncoder);
+        _intake.burnFlash();
     }
-
+    @Override
+    public void periodic() {
+      SmartDashboard.putNumber("Intake Velocity", _intakeEncoder.getVelocity());
+      SmartDashboard.putNumber("Intake Position", _intakeEncoder.getPosition());
+    }
     public void runIn() {
         var feedForwardVolts = -_ks-(_runRpm*_kv);
-        _intakePID.setReference(-_runRpm, ControlType.kSmartVelocity, 0, feedForwardVolts, ArbFFUnits.kVoltage);
+        //_intakePID.setReference(-_runRpm, ControlType.kSmartVelocity, 0, feedForwardVolts, ArbFFUnits.kVoltage);
+        _intakePID.setReference(0.2, ControlType.kDutyCycle);
     }
 
     public void runOut() {
         var feedForwardVolts = _ks+(_runRpm*_kv);
-        _intakePID.setReference(_runRpm, ControlType.kSmartVelocity, 0, feedForwardVolts, ArbFFUnits.kVoltage);
+        //_intakePID.setReference(_runRpm, ControlType.kSmartVelocity, 0, feedForwardVolts, ArbFFUnits.kVoltage);
+        _intakePID.setReference(-0.2, ControlType.kDutyCycle);
+    }
+
+    public void stop() {
+        _intake.stopMotor();
     }
     
 }
