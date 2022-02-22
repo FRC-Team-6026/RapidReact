a907920e45cbba480078ecc3d9f232702a5341e5
TigerStrike6026 <tigerstrike6026@gmail.com>
2022-02-22 17:05:03 -0600

    Added other intake motors

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -15,46 +15,66 @@ public class Intake extends SubsystemBase {
     private final double _runRpm = 2000;
     private final double _kv = 0.060519 / 60;
     private final double _ks = 0.20893;
-    private final CANSparkMax _intake = new CANSparkMax(5, MotorType.kBrushless);
+
+    private final CANSparkMax _armIntake = new CANSparkMax(5, MotorType.kBrushless);
+    private final RelativeEncoder _armIntakeEncoder;
+    private final SparkMaxPIDController _armIntakePID;
+
+    private final CANSparkMax _intake = new CANSparkMax(6, MotorType.kBrushless);
     private final RelativeEncoder _intakeEncoder;
     private final SparkMaxPIDController _intakePID;
 
+    private final CANSparkMax _conveyor = new CANSparkMax(6, MotorType.kBrushless);
+    private final RelativeEncoder _conveyorEncoder;
+    private final SparkMaxPIDController _conveyorPID;
     public Intake() {
         super();
+        _armIntakePID = _armIntake.getPIDController();
+        _armIntakeEncoder = _armIntake.getEncoder();
+        initializeMotor(_armIntake, _armIntakeEncoder, _armIntakePID, 9.9E-08, true);
 
-        _intake.restoreFactoryDefaults();
-        _intake.setIdleMode(IdleMode.kCoast);
-        _intake.setInverted(true);
         _intakePID = _intake.getPIDController();
         _intakeEncoder = _intake.getEncoder();
-        _intakePID.setP(9.9E-08);
-        _intakePID.setOutputRange(-1, 1);
-        _intakePID.setSmartMotionMaxVelocity(500, 0);
-        _intakePID.setSmartMotionMaxAccel(100, 0);
-        _intakePID.setFeedbackDevice(_intakeEncoder);
-        _intake.burnFlash();
+        initializeMotor(_intake, _intakeEncoder, _intakePID, 9.9E-08, true);
+
+        _conveyorPID = _conveyor.getPIDController();
+        _conveyorEncoder = _conveyor.getEncoder();
+        initializeMotor(_conveyor, _conveyorEncoder, _conveyorPID, 9.9E-08, true);
     }
     @Override
     public void periodic() {
-      SmartDashboard.putNumber("Intake Velocity", _intakeEncoder.getVelocity());
-      SmartDashboard.putNumber("Intake Position", _intakeEncoder.getPosition());
+      SmartDashboard.putNumber("Intake Velocity", _armIntakeEncoder.getVelocity());
+      SmartDashboard.putNumber("Intake Position", _armIntakeEncoder.getPosition());
     }
     public void runOut() {
         var feedForwardVolts = -_ks-(_runRpm*_kv);
-        _intakePID.setReference(-_runRpm, ControlType.kSmartVelocity, 0, feedForwardVolts, ArbFFUnits.kVoltage);
+        _armIntakePID.setReference(-_runRpm, ControlType.kSmartVelocity, 0, feedForwardVolts, ArbFFUnits.kVoltage);
         SmartDashboard.putNumber("Feed forward voltage", feedForwardVolts);
-        //_intakePID.setReference(0.2, ControlType.kDutyCycle);
     }
 
     public void runIn() {
         var feedForwardVolts = _ks+(_runRpm*_kv);
-        _intakePID.setReference(_runRpm, ControlType.kSmartVelocity, 0, feedForwardVolts, ArbFFUnits.kVoltage);
+        _armIntakePID.setReference(_runRpm, ControlType.kSmartVelocity, 0, feedForwardVolts, ArbFFUnits.kVoltage);
         SmartDashboard.putNumber("Feed forward voltage", feedForwardVolts);
-        //_intakePID.setReference(-0.2, ControlType.kDutyCycle);
     }
 
     public void stop() {
-        _intake.stopMotor();
+        _armIntake.stopMotor();
     }
     
+    private void initializeMotor (CANSparkMax motor,
+        RelativeEncoder encoder,
+        SparkMaxPIDController controller,
+        double p,
+        boolean isInverted){
+    motor.restoreFactoryDefaults();
+    motor.setIdleMode(IdleMode.kCoast);
+    motor.setInverted(isInverted);
+    controller.setP(p);
+    controller.setOutputRange(-1, 1);
+    controller.setSmartMotionMaxVelocity(2000, 0);
+    controller.setSmartMotionMaxAccel(1000, 0);
+    controller.setFeedbackDevice(encoder);
+    motor.burnFlash();
+}
 }
