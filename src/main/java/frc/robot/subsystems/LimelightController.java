ea479068cbc86058235cce9a05163813600cd61c
TigerStrike6026 <tigerstrike6026@gmail.com>
2022-04-02 14:27:47 -0500

    Tuning limelight

--- src/main/java/frc/robot/subsystems/LimelightController.java
+++ src/main/java/frc/robot/subsystems/LimelightController.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class LimelightController extends SubsystemBase {
     private final NetworkTableInstance _instance = NetworkTableInstance.getDefault();
     private final NetworkTable _table = _instance.getTable("limelight");
-    private final PIDController _targetPid = new PIDController(0.05, 0.01, 0);
+    private final PIDController _targetPid = new PIDController(0.03, 0, 0.0007);
     private final double _setpoint = 0;
 
     public LimelightController() {
@@ -20,9 +20,9 @@ public class LimelightController extends SubsystemBase {
 
     public void init(){
         _targetPid.setSetpoint(_setpoint);
-        _targetPid.setTolerance(0.1);
+        _targetPid.setTolerance(0.5);
         SendableRegistry.setName(_targetPid, "limelight Controller", "Target PID");
-        turnLightOff();
+        setCameraPipeline();
     }
 
     @Override
@@ -57,25 +57,32 @@ public class LimelightController extends SubsystemBase {
     public double[] getShotPower() {
         var shotPowers = new double[2];
         var tyEntry= _table.getEntry("ty");
-        var ty = tyEntry.getNumber(0);
+        var ty = tyEntry.getNumber(0).doubleValue();
+        var ft = 46.8 + -7.54*ty + 0.222*Math.pow(ty,2);
 
         // Shot power needs to change according to if TY is highr or lower. Higher number for TY means it is
         // closer therefore needing more backspin and less forward velocity, lower number means it is farther
         // away, and means it needs it needs more forward velocity and a little less backspin. 
 
-        shotPowers[0] = 0.7;
-        shotPowers[1] = 0.5;
+            //Shooter
+        shotPowers[0] = 0.341 + -7.36E-03*ft + 3.39E-03*Math.pow(ft, 2);
+            //Kicker
+        shotPowers[1] = 0.9; 
         return shotPowers;
     }
-        
-
-    public void turnLightOn(){
-        var ledEntry = _table.getEntry("ledMode");
-        ledEntry.setNumber(3);
+    
+    public boolean isTargeting(){
+        var pipelineEntry = _table.getEntry("pipeline");
+        return pipelineEntry.getNumber(1).intValue() == 0;
     }
 
-    public void turnLightOff(){
-        var ledEntry = _table.getEntry("ledMode");
-        ledEntry.setNumber(1);
+    public void setTargetPipeline(){
+        var pipelineEntry = _table.getEntry("pipeline");
+        pipelineEntry.setNumber(0);
+    }
+
+    public void setCameraPipeline(){
+        var pipelineEntry = _table.getEntry("pipeline");
+        pipelineEntry.setNumber(1);
     }
 }
